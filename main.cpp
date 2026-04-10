093d6eda1b3a6e96ea4e563a3398eb2a9cf3782d
Muhammad Awais <awaiss3553@gmail.com>
2026-04-10 16:45:45 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n=4;
+
+   for(char i=0;i<=n;i++){ // for lines that how much lines we want
+   char ch='A';
+    for(int j=0 ; j<=n; j++){ // and this loop for the value that we want to print is actually how many times dfhgrepeat
+        cout<<ch;
+        ch= ch+1;
+        }
+        cout<<endl;
+   }
+    return 0;
+}
